483c632f14c60d8a670dc9ff90b0a37e85ea04f6
fedog-ops <felixtmcd@outlook.com>
2022-11-01 14:45:18 +0000

    oneclick only add

--- src/components/ReviewById.jsx
+++ src/components/ReviewById.jsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import Homepage from "./Homepage";
 
+import LikeButton from "./LikeButton";
 
 const ReviewById = () => {
   const [isLoading, setIsLoading] = useState(true);
@@ -27,30 +28,9 @@ const ReviewById = () => {
     votes,
     review_body,
   } = review;
-const [likeIncrement, setLikeIncrement] = useState(0)
-const [isEmptyLikes, setIsEmptyLikes] = useState('')
-//setIsEmptyLikes(() => (votes === 0))
 
-  const handleLike = () => {
-    setLikeIncrement((currLikes) => currLikes + 1)
-    updateVotes(review_id, 1).then((data)=> {
-    }).catch((err)=>{
-     setLikeIncrement((currLikes)=>currLikes-1)
-     alert(err)
-    })
-    }
+
   
-  const handleDisike = () => {
-    
-   if(votes > 0) {updateVotes(review_id, -1)
-   setLikeIncrement(currLikes=>currLikes-1)
-   }
-    }
-    const heartFactory = () => {
-       if (votes + likeIncrement <= 0)return '💔'
-        return '❤️'.repeat(review.votes + likeIncrement)
-    }
-    
   
   if (isLoading) return <p>Loading...</p>;
   return (
@@ -60,17 +40,17 @@ const [isEmptyLikes, setIsEmptyLikes] = useState('')
       <div>Category: {category}</div>
       <div>Designer: {designer}</div>
       <div>Owner: {owner}</div>
-      <div> Created at: {review.created_at}</div>
+      <div>Created at: {review.created_at}</div>
       <img
         className="reviewPics"
         src={review.review_img_url}
         alt="Review picture"
       />
-      <div>Votes: {heartFactory()}</div>
-        <span className = 'voteBar'>
-                <button onClick={() => handleLike(1)}>❤️</button>
-                <button onClick={() => handleDisike(-1)}>🥴</button>
-        </span>
+      
+        
+    <LikeButton review={review}/>
+                
+      
       <div>Review : {review_body}</div>
     </div>
   );
